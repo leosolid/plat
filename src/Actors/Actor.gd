238b11fb6d9233f92aa53b99b6e005d027b793d0
Leandro Solidum <lsolidum@freeus.com>
2022-05-21 07:50:21 -0700

    feat: move left and right. disable vsync and limit fps to 120

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -7,4 +7,3 @@ var velocity := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -2,4 +2,9 @@ extends Actor
 
 
 func _physics_process(delta: float) -> void:
-	Input.get_action_strength("move_left")
+	var direction := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		1.0
+	)
+	velocity = speed * direction
+	velocity = move_and_slide(velocity)
