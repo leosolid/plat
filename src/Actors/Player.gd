4c93cbda7a99e1ff46422d05717c429d69bd4fa9
Leandro Solidum <lsolidum@freeus.com>
2022-05-21 09:08:09 -0700

    feat: jump

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -2,9 +2,22 @@ extends Actor
 
 
 func _physics_process(delta: float) -> void:
-	var direction := Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		1.0
+	var direction := get_direction()
+	velocity = calc_move_velocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+
+
+func get_direction() -> Vector2:
+	var y_strength := -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), y_strength
 	)
-	velocity = speed * direction
-	velocity = move_and_slide(velocity)
+
+
+func calc_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2) -> Vector2:
+	var new_velocity = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		new_velocity.y = speed.y * direction.y
+	return new_velocity
