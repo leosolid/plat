955decb15529ebbd7724616aa14a3d3da6f8cbcf
Leandro Solidum <lsolidum@freeus.com>
2022-05-22 09:54:15 -0700

    feat: stomp enemies

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -5,6 +5,12 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
+# callbacks
+func _on_StompDetector_body_entered(body) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	queue_free()
+
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
